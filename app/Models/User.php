1f6b8cb69081cb9be9655aca14a57b970dd72b00
Odole Olukayode <kaythinks@gmail.com>
2018-01-07 02:27:07 +0000

    first commit

---
+++ app/Models/User.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Eloquent as Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * Class User
+ * @package App\Models
+ * @version January 7, 2018, 2:00 am UTC
+ */
+class User extends Model
+{
+    use SoftDeletes;
+
+    public $table = 'users';
+    
+
+    protected $dates = ['deleted_at'];
+
+
+    public $fillable = [
+        'name',
+        'password',
+        'email'
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'name' => 'string',
+        'password' => 'string',
+        'email' => 'string'
+    ];
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+        'name' => 'required|string',
+        'password' => 'required',
+        'email' => 'required|email'
+    ];
+
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
+}
